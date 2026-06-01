b7197c686fd565aba7f60514f792442a0b3e876c
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-06-01 03:29:05 +0300

    Реализует слайдер

--- src/pages/product/ui/product-similar/ProductSimilar.tsx
+++ src/pages/product/ui/product-similar/ProductSimilar.tsx
@@ -1,6 +1,10 @@
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Navigation} from 'swiper/modules';
 import {ProductCard} from '@/entities/product-cards';
 import {useSimilarProducts} from '../../model/hooks';
+import 'swiper/css';
 import './ProductSimilar.css';
+
 
 interface Props {
   productId: string | undefined;
@@ -19,20 +23,33 @@ function ProductSimilar({productId}: Props) {
         <h2 className="title title--h3">Похожие товары</h2>
         <div className="product-similar__slider">
           <div className="product-similar__slider-list">
-            {similarProducts.map((product, index) => (
-              <ProductCard key={product.id} product={product} className={index < 3 ? 'is-active' : ''} />
-            ))}
+            <Swiper
+              modules={[Navigation]}
+              slidesPerView={3}
+              spaceBetween={32}
+              navigation={{
+                prevEl: '.slider-controls--prev',
+                nextEl: '.slider-controls--next',
+              }}
+            >
+              {similarProducts.map((product) => (
+                <SwiperSlide key={product.id}>
+                  <ProductCard product={product} className="is-active" />
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
+
           <button
             className="slider-controls slider-controls--prev"
             type="button"
             aria-label="Предыдущий слайд"
-            disabled
           >
             <svg width="7" height="12" aria-hidden="true">
               <use xlinkHref="#icon-arrow" />
             </svg>
           </button>
+
           <button
             className="slider-controls slider-controls--next"
             type="button"
