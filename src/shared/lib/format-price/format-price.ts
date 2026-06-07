8c3ab552c519ffe069358cc5caed8876d8b80ea4
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-06-07 20:54:25 +0300

    Изменит вывод formatPrice

--- src/shared/lib/format-price/format-price.ts
+++ src/shared/lib/format-price/format-price.ts
@@ -2,21 +2,15 @@ const LOCALE_RU = 'ru-Ru';
 const STYLE_CURRENCY = 'currency';
 const CURRENCY_RUB = 'RUB';
 
-export const formatPrice = (price: number): string => {
-  if (!price) {
-    return '';
-  }
-
-  return new Intl.NumberFormat(
-    LOCALE_RU,
-    {
-      style: STYLE_CURRENCY,
-      currency: CURRENCY_RUB,
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0,
-    },
-  )
-    .format(
-      price,
-    );
-};
+export const formatPrice = (price = 0): string => new Intl.NumberFormat(
+  LOCALE_RU,
+  {
+    style: STYLE_CURRENCY,
+    currency: CURRENCY_RUB,
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  },
+)
+  .format(
+    price,
+  );
