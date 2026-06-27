f9954b13f339e9369c0cfcc49d2bad76ebb01383
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-06-28 00:20:31 +0300

    Удалит helmet, добавит title

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
 import {Route, Routes} from 'react-router-dom';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
-import {HelmetProvider} from 'react-helmet-async';
 import {ToastContainer} from 'react-toastify';
 import dayjs from 'dayjs';
 import ru from 'dayjs/esm/locale/ru';
@@ -20,17 +19,15 @@ dayjs.locale(ru);
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <HelmetProvider>
-        <AppLayout>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path={AppRoute.Catalog} element={<Catalog />} />
-            <Route path={`${AppRoute.Catalog}/:id`} element={<Product />} />
-            <Route path={AppRoute.Cart} element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AppLayout>
-      </HelmetProvider>
+      <AppLayout>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path={AppRoute.Catalog} element={<Catalog />} />
+          <Route path={`${AppRoute.Catalog}/:id`} element={<Product />} />
+          <Route path={AppRoute.Cart} element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AppLayout>
 
       <ReactQueryDevtools initialIsOpen={false} />
 
